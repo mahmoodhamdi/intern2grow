1f2af485605944275867e2097277f919a5f18f94
mahmoodhamdi <mahmouashwah@gmail.com>
2023-11-02 06:15:44 +0200

    create app arc

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intern2grow/views/user_view.dart';
 
 void main() {
   runApp(const Intern2Grow());
@@ -10,6 +11,8 @@ class Intern2Grow extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return const MaterialApp(
+      home: UserView(),
+    );
   }
 }
